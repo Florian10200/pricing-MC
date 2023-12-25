cf3a127ea8fe781d0861a1eaca4a9ee8069d0864
huguesmaton <127525997+huguesmaton@users.noreply.github.com>
2023-12-25 23:53:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int main()
-{   Option option1(52.0, 50.0, 0.2, 2.0, 0.05);
+{   europeanOption option(52.0, 50.0, 0.2, 2.0, 0.05);
 
-    double K = option1.get_strike();
-    double S0 = option1.get_spot();
-    double sigma = option1.get_volatility();
-    double T = option1.get_maturity();
-    double r = option1.get_tx();
+    double K = option.get_strike();
+    double S0 = option.get_spot();
+    double sigma = option.get_volatility();
+    double T = option.get_maturity();
+    double r = option.get_tx();
 
     
     cout << "strike :" << K << endl;
@@ -22,16 +22,20 @@ int main()
     cout << "taux d'intéret :" << r << endl;
 
     
-    cout << option1.pricing_european_call_BS() << endl;
-    cout << option1.pricing_european_put_BS() << endl;
+    cout << option.pricing_european_call_BS() << endl;
+    cout << option.pricing_european_put_BS() << endl;
     
     
-    cout << option1.pricing_european_call_MC(10000) << endl; //convergence vers la valeur donnée par BS
-    cout << option1.pricing_european_put_MC(10000) << endl; //convergence vers la valeur donnée par BS
+    cout << option.delta_european_call() << endl;
+    cout << option.delta_european_put() << endl;
     
     
-    cout << option1.pricing_american_call_MC(1000) << endl; //un american call est équivalent à un european call
-    cout << option1.pricing_american_put_MC(1000) << endl; //un american put est un peu plus cher que un european put 
+    cout << option.pricing_european_call_MC(10000) << endl; //convergence vers la valeur donnée par BS
+    cout << option.pricing_european_put_MC(10000) << endl; //convergence vers la valeur donnée par BS
+    
+    
+    cout << option.pricing_american_call_MC(1000) << endl; //un american call est équivalent à un european call
+    cout << option.pricing_american_put_MC(1000) << endl; //un american put est un peu plus cher que un european put 
     
     
     return 0;
