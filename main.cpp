9c95da36e5b291b2c894727cea414716d34b9347
huguesmaton <127525997+huguesmaton@users.noreply.github.com>
2023-12-28 14:09:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main()
 {   europeanOption option1(52.0, 50.0, 0.2, 2.0, 0.05);
 
-    double K = option1.get_strike();
-    double S0 = option1.get_spot();
-    double sigma = option1.get_volatility();
-    double T = option1.get_maturity();
-    double r = option1.get_tx();
+    double K = option1.getstrike();
+    double S0 = option1.getspot();
+    double sigma = option1.getvolatility();
+    double T = option1.getmaturity();
+    double r = option1.gettx();
 
     
     cout << "strike :" << K << endl;
@@ -22,16 +22,16 @@ int main()
     cout << "taux d'intéret :" << r << endl;
 
     
-    cout << option1.pricing_european_call_BS() << endl;
-    cout << option1.pricing_european_put_BS() << endl;
+    cout << option1.pricingCallBS() << endl;
+    cout << option1.pricingPutBS() << endl;
     
     
-    cout << option1.delta_european_call() << endl;
-    cout << option1.delta_european_put() << endl;
+    cout << option1.deltaCall() << endl;
+    cout << option1.deltaPut() << endl;
     
     
-    cout << option1.pricing_european_call_MC(10000) << endl; //convergence vers la valeur donnée par BS
-    cout << option1.pricing_european_put_MC(10000) << endl; //convergence vers la valeur donnée par BS
+    cout << option1.pricingCallMC(10000) << endl; //convergence vers la valeur donnée par BS
+    cout << option1.pricingPutMC(10000) << endl; //convergence vers la valeur donnée par BS
     
     
     //----------------------------------------------------------------------------------------------------
@@ -39,8 +39,8 @@ int main()
     lookbackOption option2(50.0, 0.2, 0.25, 0.1);
 
     
-    cout << option2.pricing_lookback_call_MC(1000) << endl;
-    cout << option2.pricing_lookback_put_MC(1000) << endl;
+    cout << option2.pricingCallMC(1000) << endl;
+    cout << option2.pricingPutMC(1000) << endl;
     
     
     return 0;
