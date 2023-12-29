f23fe2ccb1cc884381fd1880531fe63003f30603
Florian10200 <126265027+Florian10200@users.noreply.github.com>
2023-12-29 10:24:20 +0100

    Update main_commun.cpp

--- main_commun.cpp
+++ main_commun.cpp
@@ -5,47 +5,49 @@ using namespace std;
 
 int main(){
     
-// For a european option -------------------------------------------------------
+// For a european option -----------------------------------------------------------------------------------------------------------------------------------------
 
     european_option option1(50.0, 52.0, 0.2, 2.0, 0.05);
     
-    double S0 = option1.get_spot();
-    double K = option1.get_strike();
-    double sigma = option1.get_volatility();
-    double T = option1.get_maturity();
-    double r = option1.get_tx();
+    double S0 = option1.get_spot(); // We get the spot price at time 0
+    double K = option1.get_strike(); // We get the strike price
+    double sigma = option1.get_volatility(); // We get the volatility
+    double T = option1.get_maturity(); // We get the maturity
+    double r = option1.get_tx(); // We get the interest rate
 
-    cout << "spot :" << S0 << endl;
+    cout << "spot :" << S0 << endl; // We show the different values of the option
     cout << "strike :" << K << endl;
     cout << "volatility :" << sigma << endl;
     cout << "maturity :" << T << endl;
-    cout << "taux d'intéret :" << r << endl;
+    cout << "interest rate :" << r << endl;
 
-    cout << "pricing european call with BS : " << option1.pricing_european_call_BS() << endl;
+    cout << "pricing european call with BS : " << option1.pricing_european_call_BS() << endl; // We get the price for a call and a put with Black-Scholes formula
     cout << "pricing european put with BS : " << option1.pricing_european_put_BS() << endl;
     
-    cout << "delta for a european call : " << option1.delta_european_call() << endl;
+    cout << "delta for a european call : " << option1.delta_european_call() << endl; // We calculate the delta
     cout << "delat for a european put : " << option1.delta_european_put() << endl;
     
-    cout << "pricing european call with Monte-Carlo : " << option1.pricing_european_call_MC(1000) << endl;
+    cout << "pricing european call with Monte-Carlo : " << option1.pricing_european_call_MC(1000) << endl; // We price them with Monte-Carlo as well
     cout << "pricing european put avec Monte-Carlo : " << option1.pricing_european_put_MC(1000) << endl;
 
-// For a lookback option ---------------------------------------------------------------------
+    
+// For a lookback option ----------------------------------------------------------------------------------------------------------------------------------
 
     lookback_option option2(50.0, 52.0, 0.2, 2.0, 0.05);
     
-    cout<<"pricing lookback call option avec MC : "<<option2.lookback_pricingCallMC(100)<<endl;
+    cout<<"pricing lookback call option avec MC : "<<option2.lookback_pricingCallMC(100)<<endl; // Pricing with Monte-Carlo for a call and a put
     cout<<"pricing lookback put option avec MC : "<<option2.lookback_pricingPutMC(100)<<endl;
 
 
-// For an asian option ----------------------------------------------------------------------
+// For an asian option --------------------------------------------------------------------------------------------------------------------------------
 
     asian_option option3(50.0, 52.0, 0.2, 2.0, 0.05);
 
-    cout<<"pricing asian call option avec MC : "<<option3.asian_pricingCallMC(100)<<endl;
+    cout<<"pricing asian call option avec MC : "<<option3.asian_pricingCallMC(100)<<endl; // Pricing with Monte-Carlo for a call and a put
     cout<<"pricing asian put option avec MC : "<<option3.asian_pricingPutMC(100)<<endl;
 
-// For a barrier option ------------------------------------------------------------------------
+    
+// For a barrier option -------------------------------------------------------------------------------------------------------------------------------------
 
     barrier_option option4(50.0, 52.0, 0.2, 2.0, 0.05,33.3,44.4);
     
