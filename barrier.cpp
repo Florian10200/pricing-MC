d1f456266e6269c96ba28875c34c4bf5254a3f80
Tad73 <127527810+Tad73@users.noreply.github.com>
2023-12-29 10:18:31 +0100

    Update barrier.cpp

--- barrier.cpp
+++ barrier.cpp
@@ -1,31 +1,17 @@
 #include <iostream>
 #include "barrier.h"
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+#include <random>
 using namespace std;
 
 double normalCDF(double x) { //cdf de la loi normale centrée réduite
    return 0.5 * erfc(-x * sqrt(0.5));
 }
 
-barrierOption::barrierOption(double s, double k, double t, double rate, double sig, double Q, double b){
-    this -> S0=s;
-    this -> K=k;
-    this -> T=t;
-    this -> r=rate;
-    this -> sigma=sig;
-    this -> q=Q;
-    this -> B=b;
-}
-
-double barrierOption::get_S0(){
-    return S0;
-}
-
-double normalCDF(double x) { //cdf de la loi normale centrée réduite
-   return 0.5 * erfc(-x * sqrt(0.5));
-}
-
-double generateN01() { //générateur de nombres aléatoires
+double generateN01() { //gnérateur de nombres aléatoires
     random_device rd; //initialisation avec une graine
     mt19937 generator(rd());
 
@@ -36,14 +22,28 @@ double generateN01() { //générateur de nombres aléatoires
     return nb;
 }
 
-double barrierOption::simassetPrice(){//simule le prix de l'asset
-    double dt = this->T/365;
+double barrierOption::simassetPrice(){
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double dt = T/365;
     double z = sqrt(dt) * static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
-    double ST = this->S0*exp((this->r-pow(this->sigma, 2)/2.0)*this->T + this->sigma*z);
+    double ST = S0*exp((r-pow(sig, 2)/2.0)*T + sig*z);
     return ST;
 }
 
-double barrierOption::MC2(string type, int N){//montecarlo de chat
+double barrierOption::MC2(string type, int N){
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
     double total=0.0;
     for (int i = 0; i < N; ++i) {
         double ST = simassetPrice();
@@ -51,13 +51,20 @@ double barrierOption::MC2(string type, int N){//montecarlo de chat
         total+=payoff;
     }
     double avg = total/static_cast<double>(N);
-    cout<<"Le prix de l'option avec MC2 est "<< avg*exp(-this->r*this->T)<<endl;
+    cout<<"Le prix de l'option avec MC2 est "<< avg*exp(-r*T)<<endl;
     return 0.0;
 }
 
-double barrierOption::extremumTraj(string type) {//méthode inspirée de ce que t'as fait hugues, elle renvoie le max ou le min de chaque traj
-    double maxP = this->B-1;
-    double minP = this->B+1;
+double barrierOption::extremumTraj(string type) {
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double maxP = B-1;
+    double minP = B+1;
     double S_jdt;
     double dt = 1.0 / 365.0;
     /*double epsilon = rand() / (RAND_MAX + 1.0); // Nombre aléatoire entre 0 et 1
@@ -66,7 +73,7 @@ double barrierOption::extremumTraj(string type) {//méthode inspirée de ce que 
     // Calcul du prix du sous-jacent selon le modèle de diffusion géométrique
     for (int j = 0; j < T*365; ++j) {
             double z = generateN01();
-            S_jdt = S0*exp((r-pow(sigma, 2)/2.0)*(j*dt) + sigma*sqrt(j*dt)*z);
+            S_jdt = S0*exp((r-pow(sig, 2)/2.0)*(j*dt) + sig*sqrt(j*dt)*z);
             if (S_jdt > maxP) {
                 maxP = S_jdt;
             }
@@ -82,20 +89,28 @@ double barrierOption::extremumTraj(string type) {//méthode inspirée de ce que 
     }
 }
 
-bool barrierOption::barrierTouched(string type, double z){//teste si z a atteint la barrière en fonction du type
+bool barrierOption::barrierTouched(string type, double z){
+    double B = barrier;
     if (type=="CUI" or type=="PUI" or type=="PUO" or type=="CUO"){
-        return z >= this->B;
+        return z >= B;
     }
     else {
-        return z <= this->B;
+        return z <= B;
     }
 }
 
-double barrierOption::barrierPayoff(string type, double z){//renvoie les payoff de l'option en fonction de si la barrière est touchée et du type de l'option 
-    double ST = this->S0*exp(this->r*this->T);
+double barrierOption::barrierPayoff(string type, double z){
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double ST = S0*exp(r*T);
     if (type=="CUI" or type=="CDI"){
         if(barrierTouched(type, z)){
-            return fmax(0, ST-this->K);
+            return fmax(0, ST-K);
         }
         else {
             return 0.0;
@@ -106,12 +121,12 @@ double barrierOption::barrierPayoff(string type, double z){//renvoie les payoff 
             return 0.0;
         }
         else {
-            return fmax(0, ST-this->K);
+            return fmax(0, ST-K);
         }
     }
     else if (type=="PUI" or type=="PDI"){
         if(barrierTouched(type, z)){
-            return fmax(0, this->K-ST);
+            return fmax(0, K-ST);
         }
         else {
             return 0.0;
@@ -122,13 +137,20 @@ double barrierOption::barrierPayoff(string type, double z){//renvoie les payoff 
             return 0.0;
         }
         else {
-            return fmax(0, this->K-ST);
+            return fmax(0, K-ST);
         }
     }
     return 0.0;
 }
 
 double barrierOption::MCbarrier(string type, int N){
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
     srand(static_cast<unsigned>(time(nullptr)));
     double SumPayoff = 0.0;
     for (int i = 0; i < N; ++i) {
@@ -136,75 +158,107 @@ double barrierOption::MCbarrier(string type, int N){
         double payoff = barrierPayoff(type, simulatedPrice);
         SumPayoff += payoff;
     }
-    double optionPrice = (SumPayoff / static_cast<double>(N)) * exp(-this->r * this->T);
+    double optionPrice = (SumPayoff / static_cast<double>(N)) * exp(-r * T);
     cout<<"The barrier option price using Monte-Carlo is "<< optionPrice<<endl;
     return 0.0;
 }
 
+barrierOption::barrierOption(double s, double k, double t, double rate, double sig, double Q, double b){
+    this -> spot=s;
+    this -> strike=k;
+    this -> maturity=t;
+    this -> txinteret=rate;
+    this -> volatility=sig;
+    this -> dividends=Q;
+    this -> barrier=b;
+}
+
+double barrierOption::get_S0(){
+    return spot;
+}
 
 double barrierOption::get_K(){
-    return K;
+    return strike;
 }
 
 double barrierOption::get_T(){
-    return T;
+    return maturity;
 }
 
 double barrierOption::get_B(){
-    return B;
+    return barrier;
 }
 
 double barrierOption::get_r(){
-    return r;
+    return txinteret;
 }
 
 double barrierOption::get_sigma(){
-    return sigma;
+    return volatility;
 }
 
 double barrierOption::get_q(){
-    return q;
+    return dividends;
 }
 
-
-
 double barrierOption::PriceCall(){
-    double d1 = (log(this->S0/this->K)+(this->r-this->q+pow(this->sigma,2)/2)*this->T)/(this->sigma*sqrt(this->T));
-    double d2 = d1-this->sigma*sqrt(this->T);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double d1 = (log(S0/K)+(r-q+pow(sig,2)/2)*T)/(sig*sqrt(T));
+    double d2 = d1-sig*sqrt(T);
     double Nd1=normalCDF(d1);
     double Nd2=normalCDF(d2);
-    double C = this->S0*exp(-this->q*this->T)*Nd1-this->K*Nd2*exp(-this->r*this->T);
+    double C = S0*exp(-q*T)*Nd1-K*Nd2*exp(-r*T);
     return C;
 }
 
 double barrierOption::PricePut(){
-    double d1 = (log(this->S0/this->K)+(this->r-this->q+pow(this->sigma,2)/2)*this->T)/(this->sigma*sqrt(this->T));
-    double d2 = d1-this->sigma*sqrt(this->T);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double d1 = (log(S0/K)+(r-q+pow(sig,2)/2)*T)/(sig*sqrt(T));
+    double d2 = d1-sig*sqrt(T);
     double Nd1=normalCDF(-d1);
     double Nd2=normalCDF(-d2);
-    double P = -(this->S0*exp(-this->q*this->T)*Nd1-this->K*Nd2*exp(-this->r*this->T));
+    double P = -(S0*exp(-q*T)*Nd1-K*Nd2*exp(-r*T));
     return P;
 }
 
 double barrierOption::Call_DownAndIn_DownAndOut(){
-    double lam = (this->r-this->q+pow(this->sigma,2)/2)/pow(this->sigma,2);
-    double y = log(pow(this->B,2)/(this->S0*this->K))/(this->sigma*sqrt(this->T))+lam*this->sigma*sqrt(this->T);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double lam = (r-q+pow(sig,2)/2)/pow(sig,2);
+    double y = log(pow(B,2)/(S0*K))/(sig*sqrt(T))+lam*sig*sqrt(T);
     double Ny=normalCDF(y);
-    double Ny2=normalCDF(y-this->sigma*sqrt(this->T));
+    double Ny2=normalCDF(y-sig*sqrt(T));
     double C = PriceCall();
-    double x1 = (log(this->S0/this->B)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
-    double y1 = (this->B/log(this->S0)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
+    double x1 = (log(S0/B)+lam*pow(sig,2)*T)/(sig*sqrt(T));
+    double y1 = (B/log(S0)+lam*pow(sig,2)*T)/(sig*sqrt(T));
     double Nx1 = normalCDF(x1);
-    double Nx11=normalCDF(x1-this->sigma*sqrt(this->T));
+    double Nx11=normalCDF(x1-sig*sqrt(T));
     double Ny1 = normalCDF(y1);
-    double Ny11=normalCDF(y1-this->sigma*sqrt(this->T));
-    if (this->B <= this->K){
-        double cdi= this->S0*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*Ny-Ny2*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+    double Ny11=normalCDF(y1-sig*sqrt(T));
+    if (B <= K){
+        double cdi= S0*exp(-q*T)*pow(B/S0,2*lam)*Ny-Ny2*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the CDI is "<< cdi <<endl;
         cout<<"The value of the CDO is "<<(C-cdi)<<endl;
     }
     else {
-        double cdo=this->S0*exp(-this->q*this->T)*Nx1-Nx11*this->K*exp(-this->r*this->T)-this->S0*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*Ny1+Ny11*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+        double cdo=S0*exp(-q*T)*Nx1-Nx11*K*exp(-r*T)-S0*exp(-q*T)*pow(B/S0,2*lam)*Ny1+Ny11*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the CDI is "<< (C-cdo) <<endl;
         cout<<"The value of the CDO is "<< cdo <<endl;
     }
@@ -213,20 +267,26 @@ double barrierOption::Call_DownAndIn_DownAndOut(){
 }
 
 double barrierOption::Call_UpAndIn_UpAndOut(){
-    // variable d1,d2,d3,d4
-    double lam = (this->r-this->q+pow(this->sigma,2)/2)/pow(this->sigma,2);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double lam = (r-q+pow(sig,2)/2)/pow(sig,2);
     double C = PriceCall();
-    double x1 = (log(this->S0/this->B)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
-    double y = log(pow(this->B,2)/(this->S0*this->K))/(this->sigma*sqrt(this->T))+lam*this->sigma*sqrt(this->T);
-    double y1 = (this->B/log(this->S0)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
+    double x1 = (log(S0/B)+lam*pow(sig,2)*T)/(sig*sqrt(T));
+    double y = log(pow(B,2)/(S0*K))/(sig*sqrt(T))+lam*sig*sqrt(T);
+    double y1 = (B/log(S0)+lam*pow(sig,2)*T)/(sig*sqrt(T));
     double Ny=normalCDF(-y);
-    double Nyy=normalCDF(-y+this->sigma*sqrt(this->T));
+    double Nyy=normalCDF(-y+sig*sqrt(T));
     double Ny1=normalCDF(-y1);
-    double Ny11=normalCDF(-y1+this->sigma*sqrt(this->T));
+    double Ny11=normalCDF(-y1+sig*sqrt(T));
     double Nx1 = normalCDF(x1);
-    double Nx11=normalCDF(x1-this->sigma*sqrt(this->T));
-    if (this->B > this->K){
-        double cui=this->S0*exp(-this->q*this->T)*Nx1-Nx11*this->K*exp(-this->r*this->T)-this->S0*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*(Ny-Ny1)+(Nyy-Ny11)*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+    double Nx11=normalCDF(x1-sig*sqrt(T));
+    if (B > K){
+        double cui=S0*exp(-q*T)*Nx1-Nx11*K*exp(-r*T)-S0*exp(-q*T)*pow(B/S0,2*lam)*(Ny-Ny1)+(Nyy-Ny11)*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the CUI is "<< cui <<endl;
         cout<<"The value of the CUO is "<< (C-cui)<<endl;
     }
@@ -238,24 +298,31 @@ double barrierOption::Call_UpAndIn_UpAndOut(){
 }
 
 double barrierOption::Put_UpAndIn_UpAndOut(){
-    double lam = (this->r-this->q+pow(this->sigma,2)/2)/pow(this->sigma,2);
-    double y = log(pow(this->B,2)/(this->S0*this->K))/(this->sigma*sqrt(this->T))+lam*this->sigma*sqrt(this->T);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double lam = (r-q+pow(sig,2)/2)/pow(sig,2);
+    double y = log(pow(B,2)/(S0*K))/(sig*sqrt(T))+lam*sig*sqrt(T);
     double Ny = normalCDF(-y);
-    double Nyy = normalCDF(-y+this->sigma*sqrt(this->T));
-    double x1 = (log(this->S0/this->B)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
-    double y1 = (this->B/log(this->S0)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
+    double Nyy = normalCDF(-y+sig*sqrt(T));
+    double x1 = (log(S0/B)+lam*pow(sig,2)*T)/(sig*sqrt(T));
+    double y1 = (B/log(S0)+lam*pow(sig,2)*T)/(sig*sqrt(T));
     double Nx1 = normalCDF(-x1);
     double Ny1 = normalCDF(-y1);
-    double Nx11=normalCDF(-x1+this->sigma*sqrt(this->T));
-    double Ny11=normalCDF(-y1+this->sigma*sqrt(this->T));
+    double Nx11=normalCDF(-x1+sig*sqrt(T));
+    double Ny11=normalCDF(-y1+sig*sqrt(T));
     double P = PricePut();
-    if (this->B >= this->K){
-        double pui = this->S0*(-1)*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*Ny+Nyy*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+    if (B >= K){
+        double pui = S0*(-1)*exp(-q*T)*pow(B/S0,2*lam)*Ny+Nyy*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the PUI is "<< pui <<endl;
         cout<<"The value of the PUO is "<< (P-pui)<<endl;
     }
     else{
-        double puo = this->S0*(-1)*exp(-this->q*this->T)*Nx1+Nx11*this->K*exp(-this->r*this->T)+this->S0*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*Ny1-Ny11*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+        double puo = S0*(-1)*exp(-q*T)*Nx1+Nx11*K*exp(-r*T)+S0*exp(-q*T)*pow(B/S0,2*lam)*Ny1-Ny11*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the PUI is "<< (P-puo) <<endl;
         cout<<"The value of the PUO is "<< puo <<endl;
     }
@@ -263,19 +330,26 @@ double barrierOption::Put_UpAndIn_UpAndOut(){
 }
 
 double barrierOption::Put_DownAndIn_DownAndOut(){
-    double lam = (this->r-this->q+pow(this->sigma,2)/2)/pow(this->sigma,2);
+    double T = maturity;
+    double S0 = spot;
+    double K = strike;
+    double sig = volatility;
+    double r = txinteret;
+    double q = dividends;
+    double B = barrier;
+    double lam = (r-q+pow(sig,2)/2)/pow(sig,2);
     double P = PricePut();
-    double x1 = (log(this->S0/this->B)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
-    double y1 = (this->B/log(this->S0)+lam*pow(this->sigma,2)*this->T)/(this->sigma*sqrt(this->T));
-    double y = log(pow(this->B,2)/(this->S0*this->K))/(this->sigma*sqrt(this->T))+lam*this->sigma*sqrt(this->T);
+    double x1 = (log(S0/B)+lam*pow(sig,2)*T)/(sig*sqrt(T));
+    double y1 = (B/log(S0)+lam*pow(sig,2)*T)/(sig*sqrt(T));
+    double y = log(pow(B,2)/(S0*K))/(sig*sqrt(T))+lam*sig*sqrt(T);
     double Nx1=normalCDF(-x1);
-    double Nx11 = normalCDF(-x1+this->sigma*sqrt(this->T));
+    double Nx11 = normalCDF(-x1+sig*sqrt(T));
     double Ny = normalCDF(y);
     double Ny1 = normalCDF(y1);
-    double Nyy = normalCDF(y-this->sigma*sqrt(this->T));
-    double Ny11 = normalCDF(y1-this->sigma*sqrt(this->T));
-    if (this->B <= this->K){
-        double pdi = this->S0*(-1)*exp(-this->q*this->T)*Nx1+Nx11*this->K*exp(-this->r*this->T)+this->S0*exp(-this->q*this->T)*pow(this->B/this->S0,2*lam)*(Ny-Ny1)-(Nyy-Ny11)*this->K*pow(this->B/this->S0,2*lam-2)*exp(-this->r*this->T);
+    double Nyy = normalCDF(y-sig*sqrt(T));
+    double Ny11 = normalCDF(y1-sig*sqrt(T));
+    if (B <= K){
+        double pdi = S0*(-1)*exp(-q*T)*Nx1+Nx11*K*exp(-r*T)+S0*exp(-q*T)*pow(B/S0,2*lam)*(Ny-Ny1)-(Nyy-Ny11)*K*pow(B/S0,2*lam-2)*exp(-r*T);
         cout<<"The value of the PDI is "<< pdi <<endl;
         cout<<"The value of the PDO is "<< (P-pdi) <<endl;
     }
