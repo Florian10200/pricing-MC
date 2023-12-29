a9361c52c988c1b2e096d45406dd4eb9a93949bb
Florian10200 <126265027+Florian10200@users.noreply.github.com>
2023-12-29 10:24:52 +0100

    Update commun.h

--- commun.h
+++ commun.h
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-double normalCDF(double x);
+double normalCDF(double x); // Cumulative distribution fonction of the N(0,1)
+double generateN01(); // Generate a distribution of N(0,1)
 
-double generateN01(); 
 
 
-class option {
+class option { // General class for all the options
     private:
         double spot; //in $
         double strike; //in $
@@ -17,57 +17,57 @@ class option {
         double maturity; //in years
         double txinteret; //in %
     public:
-        option(double S_t, double K, double vol, double maturity, double tx); //constructeur
-        double get_strike(); //recupere la valeur du strike
-        double get_spot(); //recupere la valeur du spot
-        double get_volatility(); //recupere la valeur de la volatilite
-        double get_maturity(); //recupere la valeur de la maturite
-        double get_tx(); //recupere la valeur du tx d'interet
+        option(double S_t, double K, double vol, double maturity, double tx); //constructor
+        double get_strike(); //get the strike price
+        double get_spot(); //get the spot price
+        double get_volatility(); //get the volatility
+        double get_maturity(); //get the maturity
+        double get_tx(); //get the interest rate
 };        
 
-// European option Pricing par les formules analytiques et par Monte
+// European option Pricing with analytic formula and Monte-Carlo---------------------------------------------------------------
 
-class european_option : public option {
+class european_option : public option { // Heritage from the class option
     public:
         european_option(double S_t, double K, double vol, double maturity, double tx);
         
-        double pricing_european_call_BS(); //pricing european call par BS
-        double pricing_european_put_BS(); //pricing european put par BS
+        double pricing_european_call_BS(); //pricing european call with BS
+        double pricing_european_put_BS(); //pricing european put with BS
     
-        double delta_european_call(); //calcul du delta pour un european call
-        double delta_european_put(); //calcul du delta pour un european put
+        double delta_european_call(); //Delta for a european call
+        double delta_european_put(); //Delta for a european put
         
-        double pricing_european_call_MC(int N); //pricing european call par MC, N le nombre de trajectoires
-        double pricing_european_put_MC(int N); //pricing european put par MC, N le nombre de trajectoires
+        double pricing_european_call_MC(int N); //pricing european call with MC, N the number of path
+        double pricing_european_put_MC(int N); //pricing european put with MC, N the number of path
 };
 
-// For a lookback option avec Monte-Carlo
+// For a lookback option with Monte-Carlo-----------------------------------------------------------------------------------------
 
 
 class lookback_option : public option {
     public:
         lookback_option(double S_t, double K, double vol, double maturity, double tx);
         
-        double lookback_pricingCallMC(int N); //pricing lookback call par MC, N le nombre de trajectoires
-        double lookback_pricingPutMC(int N); //pricing lookback put par MC, N le nombre de trajectoires
+        double lookback_pricingCallMC(int N); //pricing lookback call with MC, N the number of path
+        double lookback_pricingPutMC(int N); //pricing lookback put with MC, N the number of path
         
 };
 
 
-// For an asian option with Monte-Carlo
+// For an asian option with Monte-Carlo-------------------------------------------------------------------------------------
 
 class asian_option : public option {
     public:
         asian_option(double S_t, double K, double vol, double maturity, double tx);
     
-        double asian_pricingCallMC(int N); // Pricing d'une asian call option par MC, N lo nombre de trajectoire
-        double asian_pricingPutMC(int N); // Pricing d'une asian put option par MC, N lo nombre de trajectoire
+        double asian_pricingCallMC(int N); // Pricing asian call option with MC, N the number of path
+        double asian_pricingPutMC(int N); // Pricing asian put option with MC, N the number of path
 };
 
 
-// For a barrier option_pricing_h
+// For a barrier option------------------------------------------------------------------------------------------------
 
-class barrier_option : public option {
+class barrier_option : public option { // The Barrier option class has two more arguments : the Barrier B and the dividends q
     private:
         double q;
         double B;
@@ -75,6 +75,9 @@ class barrier_option : public option {
         barrier_option(double S_t, double K, double vol, double maturity, double tx, double q, double B);
         double get_q();
         double get_B();
+        double Call_UpAndIn_UpAndOut();
+        double Call_DownAndIn_DownAndOut();
+        double Put_UpAndIn_UpAndOut();
 };
 
 
